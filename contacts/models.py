775b6ce78a26cb68fcdaed962c59a0abe1650042
Alif Jahan <sajeeb162537@gmail.com>
2019-07-10 17:14:15 +0600

    Fixed API view classes

--- contacts/models.py
+++ contacts/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Contact(models.Model):
 
-    contact_id = models.IntegerField(primary_key=True)
     contact_name = models.CharField(max_length=30)
+    contact_phone = models.IntegerField()
+    contact_email = models.EmailField(max_length=50)
+    contact_address = models.TextField(max_length=120, default='')
 
     def __str__(self):
         return self.contact_name
