c2c764e081a738a7f3f91567e7b3599e9e2d09c6
Alif Jahan <alif@interconnectionbd.com>
2019-09-27 19:08:19 +0600

    Created ContactType serialzier

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from contacts.models import Contact
+from contacts.models import Contact, ContactType
+
+
+class ContactTypeSerializer(serializers.ModelSerializer):
+    """Serailizer for Contact Model"""
+
+    class Meta:
+        model = ContactType
+        fields = '__all__'
 
 
 class ContactSerializer(serializers.ModelSerializer):
     """Serailizer for Contact Model"""
+
+    contact_type = ContactTypeSerializer()
 
     class Meta:
         model = Contact
