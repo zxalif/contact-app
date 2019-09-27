f31e47aaf7fe30d2d335d6bdca04164efad3f2e1
Alif Jahan <alif@interconnectionbd.com>
2019-09-27 19:10:23 +0600

    Added api view for ContactType

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,8 @@
 from django.views.generic.base import TemplateView
 from rest_framework.generics import (ListCreateAPIView,
                                      RetrieveUpdateDestroyAPIView)
-from contacts.models import Contact
-from contacts.serializers import ContactSerializer
+from contacts.models import Contact, ContactType
+from contacts.serializers import ContactSerializer, ContactTypeSerializer
 
 
 class IndexView(TemplateView):
@@ -21,3 +21,17 @@ class ContactDetailsView(RetrieveUpdateDestroyAPIView):
 
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+
+class ContactTypeListView(ListCreateAPIView):
+    """Class for Create and Retrieve Contact data"""
+
+    queryset = ContactType.objects.all()
+    serializer_class = ContactTypeSerializer
+
+
+class ContactTypeDetailsView(RetrieveUpdateDestroyAPIView):
+    """Class for Update, Retrieve and Delete Contact data"""
+
+    queryset = ContactType.objects.all()
+    serializer_class = ContactTypeSerializer
